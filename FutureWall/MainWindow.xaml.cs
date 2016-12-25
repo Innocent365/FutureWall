a652a7be757be4823bf9d70d1143e539a0bad82d
VincentAry <cc531181290@gmail.com>
2016-12-26 01:29:56 +0800

    nearly finish

--- FutureWall/MainWindow.xaml.cs
+++ FutureWall/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FutureWall
 {
@@ -33,6 +21,10 @@ namespace FutureWall
 
         private void OnLoad(object sender, RoutedEventArgs e)
         {
+//            var page = new VisualDemo();
+//            Content = page;
+//            return;
+
             var example = new AnimationPage();
             example.Width = 800;
             example.Height = 600;
